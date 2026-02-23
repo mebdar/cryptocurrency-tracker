c78529956fcad9930f99f5a9e95c09f1c1265f6a
Bireh <birehkeith@gmail.com>
2026-02-23 09:51:50 +0300

    market page

--- src/pages/Markets.jsx
+++ src/pages/Markets.jsx
@@ -2,7 +2,7 @@ import SalesChart from "../components/SalesChart";
 
 export default function Markets() {
     return (
-        <div style={{ padding: "20px", display: "flex", justifyContent: "center", alignItems: "center", minHeight: "80vh" }}>
+        <div style={{ padding: "20px", display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
             <div style={{ width: "100%", maxWidth: "1000px" }}>
                 <SalesChart />
             </div>
